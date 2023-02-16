0b5ea55ba66ce119b06383c78db18b1f1d567d23
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-02-16 13:57:53 -0800

    This MLJ.jl... is too powerful :scream:

--- pythonFullCourseForBeginners/firstCode.py
+++ pythonFullCourseForBeginners/firstCode.py
@@ -1,1 +1,12 @@
-print("Hello World!\n")
+character_name = "John";
+character_age = 35;
+print("There was once a person named " + character_name + \
+      "who was " + str(character_age) + " years old.\n")
+
+friends = ["Bheem", "Raju", "Chutki", "Dholu", "Bholu"]
+friends[6:6] = ["Ajatshatru"]
+# friends[len(friends):len(friends)] = ["Daku Mangal Singh"]
+# friends.append("Daku Mangal Singh")
+# friends.append("Kalia", "Jaggu")
+# friends[1:2:5] = ["x"]
+print(friends[1:2:3])
