dd166b2bfc8f9730351c601cd1006b6e92e66c23
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-03-20 14:18:28 -0700

    rewriting the function in a way I understand.

--- dynamicProgramming_freeCodeCamp/gridTraveler.js
+++ dynamicProgramming_freeCodeCamp/gridTraveler.js
@@ -1,14 +1,15 @@
-const gridTraveler = (rows, cols, memo = {}) => {
+function numPathways(rows, cols, memo = {}){
     const key = rows + ',' + cols;
     if (key in memo) return memo[key];    
     if (rows === 1 && cols === 1) return 1;
     if (rows === 0 || cols === 0) return 0;
-    memo[key] =  gridTraveler(rows-1, cols, memo) + gridTraveler(rows, cols-1, memo);
+    memo[key] =  numPathways(rows-1, cols, memo) + numPathways(rows, cols-1, memo);
     return memo[key];
 }   
 
-console.log(gridTraveler(1, 1));
-console.log(gridTraveler(2, 3));
-console.log(gridTraveler(3, 2));
-console.log(gridTraveler(3, 3));
-console.log(gridTraveler(18, 18));
+console.log(numPathways(1, 1));
+console.log(numPathways(2, 3));
+console.log(numPathways(3, 2));
+console.log(numPathways(3, 3));
+console.log(numPathways(18, 18));
+//without memoization, the last computation is very slow.
