34f87dbfdb99ccc554480eee80e04845be0525c4
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-11 08:08:30 -0700

    symbolic function and gradient obtained

--- Symbolics/main.jl
+++ Symbolics/main.jl
@@ -7,7 +7,7 @@ using Symbolics
 
 M = [t+2t^2 6t; x+2t+2y y^2];
 
-f, f! = build_function(M, [x, y, t], expression=Val{false);
+f, f! = build_function(M, [x, y, t], expression=Val{false});
 # putting that third argument helped me bypass an error
 # I don't truly understand what that does
 
@@ -22,4 +22,12 @@ f, f! = build_function(M, [x, y, t], expression=Val{false);
         # something from inside let block to an outside variable
 end;
 
+function objFun(;getGradientToo=true)
+        @variables A₀, A, τ, ω, α, ϕ
+        x = [A₀, A, τ, ω, α, ϕ] 
+        f = A₀ + A*exp(-t/τ)sin((ω+α*t)t + ϕ)
+        ∇f = Symbolics.gradient(f, x)
+        return f, ∇f
+end
 
+f, ∇f = objFun();
