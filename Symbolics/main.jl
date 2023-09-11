bfc8aa8fe8a24e8680b54866edf49e765d2141cc
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-10 23:25:18 -0700

    let block is a thing

--- Symbolics/main.jl
+++ Symbolics/main.jl
@@ -1,16 +1,25 @@
+using BenchmarkTools
 using LinearAlgebra
 using Revise
 using Symbolics
 
-@variables t x y u(..); # u is a real valued function. x, y, t are real values
+@variables t x y;
 
 M = [t+2t^2 6t; x+2t+2y y^2];
 
-f, f! = build_function(M, [x, y, t])
-func_f = eval(f)
-result = f(1.0, 2.0, 3.0)
+f, f! = build_function(M, [x, y, t], expression=Val{false);
+# putting that third argument helped me bypass an error
+# I don't truly understand what that does
 
-let ef = eval(f), ef! = eval(f!), M = zeros(2, 2), inputs = [1.2, 3.4, 5.6]
-        ef!(M, inputs)
-        ef(inputs), M
-end
+@btime answer = let
+        ef = eval(f)
+        ef! = eval(f!)
+        M = zeros(2, 2)
+        inputs = [1.2, 3.4, 5.6]
+        A = Float64[5 6; 7 8]
+        ef!(A, inputs)
+        (output=ef(inputs), M=M, A=A) # return not useful if you want to save
+        # something from inside let block to an outside variable
+end;
+
+
